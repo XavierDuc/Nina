dcb95fc0e9046b23493488bfd45b270ce9e5045e
ZoonyMac <zoonymac@ZoonyMacs-MacBook-Pro.local>
2024-11-28 09:49:39 +0100

    format images webp & third party code reduces & paint element

--- ninacarducci.github.io-main/assets/scripts.js
+++ ninacarducci.github.io-main/assets/scripts.js
@@ -1,15 +1,17 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = document.querySelector(".gallery");
+
+  mauGallery(gallery, {
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  });
 });
